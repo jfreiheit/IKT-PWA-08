1e0b72859c316158675628a7d0790b10a397d38b
J. Freiheit <freiheit@htw-berlin.de>
2022-06-16 11:12:08 +0200

    vor push notifications

---
+++ public/src/js/db.js
@@ -0,0 +1,63 @@
+
+const db = idb.openDB('posts-store', 2, {
+    upgrade(db) {
+        // Create a store of objects
+        const store1 = db.createObjectStore('posts', {
+            // The '_id' property of the object will be the key.
+            keyPath: '_id',
+            // If it isn't explicitly set, create a value by auto incrementing.
+            autoIncrement: true,
+        });
+        // Create an index on the '_id' property of the objects.
+        store1.createIndex('_id', '_id');
+        
+        // Create another store of objects
+        const store2 = db.createObjectStore('sync-posts', {
+            keyPath: 'id',
+            autoIncrement: true,
+        });
+        store2.createIndex('id', 'id');
+    },
+});
+
+function writeData(st, data) {
+    return db
+    .then( dbPosts => {
+        let tx = dbPosts.transaction(st, 'readwrite');
+        let store = tx.objectStore(st);
+        store.put(data);
+        return tx.done;
+    })
+}
+
+function readAllData(st) {
+    return db
+        .then( dbPosts => {
+            let tx = dbPosts.transaction(st, 'readonly');
+            let store = tx.objectStore(st);
+            return store.getAll();
+        })
+}
+
+function clearAllData(st) {
+    return db
+        .then( dbPosts => {
+            let tx = dbPosts.transaction(st, 'readwrite');
+            let store = tx.objectStore(st);
+            store.clear();
+            return tx.done;
+        })
+}
+
+function deleteOneData(st, _id) {
+    db
+    .then( dbPosts => {
+        let tx = dbPosts.transaction(st, 'readwrite');
+        let store = tx.objectStore(st);
+        store.delete(_id);
+        return tx.done;
+    })
+    .then( () => {
+        console.log('Data deleted ...');
+    });
+}
---
+++ public/sw.js
@@ -0,0 +1,131 @@
+importScripts('/src/js/idb.js');
+importScripts('/src/js/db.js');
+
+const CACHE_VERSION = 1;
+const CURRENT_STATIC_CACHE = 'static-v'+CACHE_VERSION;
+const CURRENT_DYNAMIC_CACHE = 'dynamic-v'+CACHE_VERSION;
+
+self.addEventListener('install', event => {
+    console.log('service worker --> installing ...', event);
+    event.waitUntil(
+        caches.open(CURRENT_STATIC_CACHE)
+            .then( cache => {
+                console.log('Service-Worker-Cache erzeugt und offen');
+                cache.addAll([
+                    '/',
+                    '/index.html',
+                    '/src/js/app.js',
+                    '/src/js/feed.js',
+                    '/src/js/material.min.js',
+                    '/src/js/idb.js',
+                    '/src/css/app.css',
+                    '/src/css/feed.css',
+                    '/src/images/htw.jpg',
+                    'https://fonts.googleapis.com/css?family=Roboto:400,700',
+                    'https://fonts.googleapis.com/icon?family=Material+Icons',
+                    'https://code.getmdl.io/1.3.0/material.blue_grey-red.min.css'
+                ]);
+            })
+    );
+})
+
+self.addEventListener('activate', event => {
+    console.log('service worker --> activating ...', event);
+    event.waitUntil(
+        caches.keys()
+            .then( keyList => {
+                return Promise.all(keyList.map( key => {
+                    if(key !== CURRENT_STATIC_CACHE && key !== CURRENT_DYNAMIC_CACHE) {
+                        console.log('service worker --> old cache removed :', key);
+                        return caches.delete(key);
+                    }
+                }))
+            })
+    );
+    return self.clients.claim();
+})
+
+self.addEventListener('fetch', event => {
+    // check if request is made by chrome extensions or web page
+    // if request is made for web page url must contains http.
+    if (!event.request.url.includes('http')) return;        // skip the request. if request is not made with http protocol
+    if (event.request.url.includes('myFile.jpg')) return;   // skip the request. see feed.js fetch(imageURI)
+
+    const url = 'http://localhost:3000/posts';
+    if(event.request.url.indexOf(url) >= 0) {
+        console.log('event.request', event.request)
+        event.respondWith(
+            fetch(event.request)
+                .then ( res => {
+                    if(event.request.method === 'GET') {
+                        const clonedResponse = res.clone();
+                        clearAllData('posts')
+                        .then( () => {
+                            clonedResponse.json()
+                            .then( data => {
+                                for(let key in data)
+                                {
+                                    writeData('posts', data[key]);
+                                }
+                            })
+                        })
+                    }
+                    return res;
+                })
+        )
+    } else {
+        event.respondWith(
+            caches.match(event.request)
+                .then( response => {
+                    if(response) {
+                        return response;
+                    } else {
+                        console.log('event.request', event.request)
+                        return fetch(event.request)
+                            .then( res => {     // nicht erneut response nehmen, haben wir schon
+                                return caches.open(CURRENT_DYNAMIC_CACHE)      // neuer, weiterer Cache namens dynamic
+                                    .then( cache => {
+                                        cache.put(event.request.url, res.clone());
+                                        return res;
+                                    })
+                            });
+                    }
+                })
+        )
+    }
+})
+
+self.addEventListener('sync', event => {
+    console.log('service worker --> background syncing ...', event);
+    if(event.tag === 'sync-new-post') {
+        console.log('service worker --> syncing new posts ...');
+        event.waitUntil(
+            readAllData('sync-posts')
+                .then( dataArray => {
+                    for(let data of dataArray) {
+                        console.log('data from IndexedDB', data);
+                        const formData = new FormData();
+                        formData.append('title', data.title);
+                        formData.append('location', data.location);
+                        formData.append('file', data.image_id);
+
+                        console.log('formData', formData)
+                    
+                        fetch('http://localhost:3000/posts', {
+                            method: 'POST',
+                            body: formData
+                        })
+                        .then( response => {
+                            console.log('Data sent to backend ...', response);
+                            if(response.ok) {
+                                deleteOneData('sync-posts', data.id)
+                            }
+                        })
+                        .catch( err => {
+                            console.log('Error while sending data to backend ...', err);
+                        })
+                    }
+                })
+        );
+    }
+})
